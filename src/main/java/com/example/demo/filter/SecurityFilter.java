3b5b1f658494505b239419a60ec8135db208d858
nebula-2020 <2781536086@qq.com>
2021-05-25 11:26:35 +0800

    feat:完善权限过滤器
    修改1个过滤器

--- src/main/java/com/example/demo/filter/SecurityFilter.java
+++ src/main/java/com/example/demo/filter/SecurityFilter.java
@@ -1,7 +1,9 @@
 package com.example.demo.filter;
 
 import java.io.IOException;
+import java.util.*;
 
+import com.alibaba.fastjson.JSON;
 import com.example.demo.constants.Const;
 
 import org.apache.commons.lang3.StringUtils;
@@ -12,38 +14,78 @@ import jakarta.servlet.http.*;
 public class SecurityFilter implements Filter
 {
 
-    private static String[] suffix;
-
-    public SecurityFilter()
-    {
-
-        if (suffix == null)
-        {
-            suffix = new String[Const.STATICS.length];
-
-            for (int i = 0; i < Const.STATICS.length; i++)
-            {
-                suffix[i] = StringUtils.join(new String[] {
-                        Const.HOST_URL, Const.STATICS[i], ""
-                }, Const.SLASH);
-            }
-        }
-    }
-
     @Override
     public void doFilter(
             ServletRequest request, ServletResponse response, FilterChain chain
     ) throws IOException, ServletException
     {
+        List<String> suffix = new ArrayList<>();
+
+        for (int i = 0; i < Const.STATICS.length; i++)
+        {
+            suffix.add(StringUtils.join(new String[] {
+                    Const.HOST_URL, Const.STATICS[i]
+            }, Const.SLASH));
+        }
         HttpServletRequest req = (HttpServletRequest)request;
         String url = req.getRequestURL().toString();
         System.out.println(url);
+        boolean res = false;
 
-        if (!StringUtils.startsWithAny(url, suffix))
-        {// 不是静态资源，需要权限
-            Cookie[] cookie = req.getCookies();
+        if (!StringUtils
+                .startsWithAny(url, suffix.toArray(new String[suffix.size()])))
+        { // 不是静态资源，需要权限
+            System.out.println("Verify...");
 
+            try
+            {
+                Cookie[] cs = req.getCookies();
+                HttpSession session = req.getSession(false);
+
+                if (session != null && cs != null)
+                {
+                    System.out.println("Session exists.");
+
+                    for (Cookie c: cs)
+                    {
+                        System.out.println(c.getName() + "::" + c.getValue());
+
+                        if (c.getName().compareTo(Const.COOKIE_ID) == 0)
+                        {
+                            Object v = session.getAttribute(c.getValue());
+
+                            if (v != null && (Boolean)v)
+                            {
+                                System.out.println("Verify succeed.");
+                                res = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+            }
         }
-        chain.doFilter(request, response);
+        else// 静态资源
+        {
+            res = true;
+        }
+
+        if (res)
+        {
+            chain.doFilter(request, response);
+        }
+        else
+        {
+            System.out.println("Verify Failed:");
+            System.out.println("\t" + JSON.toJSONString(req.getCookies()));
+            ((HttpServletResponse)response).setStatus(403);
+        }
     }
 }
